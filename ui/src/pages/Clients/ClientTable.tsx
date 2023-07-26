01b832cdc9a4812ed11ffc3d528c603c5d310e9b
RodRivera-00 <admin@yeonin.io>
2023-07-26 14:38:43 +0800

    feat: added search filter

--- ui/src/pages/Clients/ClientTable.tsx
+++ ui/src/pages/Clients/ClientTable.tsx
@@ -1,37 +1,50 @@
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-import ClientRow from "./ClientRow";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import ClientRow from './ClientRow';
 
-export default function BasicTable({ clients }: { clients: IClient[] }) {
-  return (
-    <TableContainer component={Paper} sx={{ maxWidth: "100%" }}>
-      <Table sx={{ minWidth: 400 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Name</TableCell>
-            <TableCell>Phone number</TableCell>
-            <TableCell>Email</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {clients.map((client) => (
-            <ClientRow key={client.id} client={client} />
-          ))}
-          {!clients ||
-            (!clients.length && (
-              <TableRow sx={{ padding: 3 }}>
-                <TableCell component="th" scope="row">
-                  No clients
-                </TableCell>
-              </TableRow>
-            ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  );
+interface BasicTableProps {
+	clients: IClient[];
+	filter?: string;
+}
+
+export default function BasicTable({ clients, filter = '' }: BasicTableProps) {
+	return (
+		<TableContainer component={Paper} sx={{ maxWidth: '100%', borderRadius: '10px' }}>
+			<Table sx={{ minWidth: 400, border: '1px solid #f5f5f7', boxShadow: 'none' }} aria-label='simple table'>
+				<TableHead>
+					<TableRow>
+						<TableCell>Name</TableCell>
+						<TableCell>Phone number</TableCell>
+						<TableCell>Email</TableCell>
+					</TableRow>
+				</TableHead>
+				<TableBody>
+					{clients
+						.filter((item) => {
+							return (
+								item.firstName.toLowerCase().includes(filter.toLowerCase()) ||
+								item.lastName.toLowerCase().includes(filter.toLowerCase()) ||
+								`${item.firstName} ${item.lastName}`.toLowerCase().includes(filter.toLowerCase())
+							);
+						})
+						.map((client) => (
+							<ClientRow key={client.id} client={client} />
+						))}
+					{!clients ||
+						(!clients.length && (
+							<TableRow sx={{ padding: 3 }}>
+								<TableCell component='th' scope='row'>
+									No clients
+								</TableCell>
+							</TableRow>
+						))}
+				</TableBody>
+			</Table>
+		</TableContainer>
+	);
 }
--- ui/src/pages/Clients/index.tsx
+++ ui/src/pages/Clients/index.tsx
@@ -1,30 +1,79 @@
-import { memo, useContext, useEffect } from "react";
-import { Paper, Typography } from "@mui/material";
-import { StateContext } from "../../store/DataProvider";
-import Page from "../../components/Page";
-import ClientTable from "./ClientTable";
-import { getClients } from "../../services/api";
+import { memo, useContext, useEffect, useState } from 'react';
+import { Container, Paper, Typography, InputAdornment, Button, Input } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import { StateContext } from '../../store/DataProvider';
+import Page from '../../components/Page';
+import ClientTable from './ClientTable';
+import { getClients } from '../../services/api';
 
 function Clients() {
-  const { state, dispatch } = useContext(StateContext);
-  const { clients } = state;
+	const [search, setSearch] = useState('');
+	const { state, dispatch } = useContext(StateContext);
+	const { clients } = state;
 
-  useEffect(() => {
-    getClients().then((clients) =>
-      dispatch({ type: "FETCH_ALL_CLIENTS", data: clients })
-    );
-  }, [dispatch]);
+	useEffect(() => {
+		getClients().then((clients) => dispatch({ type: 'FETCH_ALL_CLIENTS', data: clients }));
+	}, [dispatch]);
 
-  return (
-    <Page>
-      <Typography variant="h4" sx={{ textAlign: "start" }}>
-        Clients
-      </Typography>
-      <Paper sx={{ margin: "auto", marginTop: 3 }}>
-        <ClientTable clients={clients} />
-      </Paper>
-    </Page>
-  );
+	return (
+		<Page>
+			<Typography variant='h4' sx={{ textAlign: 'start', fontWeight: 'bold' }}>
+				Clients
+			</Typography>
+			<Container
+				sx={{
+					width: '100%',
+					justifyContent: 'space-between',
+					alignItems: 'end',
+					display: 'flex',
+					marginTop: 3,
+				}}
+				disableGutters
+			>
+				<Input
+					placeholder='Search clients...'
+					sx={{
+						background: 'white',
+						border: '2px solid #ecedf0',
+						borderRadius: '10px',
+						paddingY: 0.5,
+						paddingX: 1.5,
+					}}
+					onChange={(e) => setSearch(e.target.value)}
+					endAdornment={
+						<InputAdornment position='end'>
+							<SearchIcon />
+						</InputAdornment>
+					}
+					disableUnderline
+				/>
+				<Button
+					size='large'
+					variant='contained'
+					sx={{
+						background: '#345fff',
+						fontSize: 12,
+						paddingY: 1.5,
+						borderRadius: '10px',
+						textTransform: 'none',
+					}}
+				>
+					Create new client
+				</Button>
+			</Container>
+			<Paper
+				sx={{
+					margin: 'auto',
+					marginTop: 3,
+					boxShadow: 'none',
+					border: '2px solid #ecedf0',
+					borderRadius: '10px',
+				}}
+			>
+				<ClientTable clients={clients} filter={search} />
+			</Paper>
+		</Page>
+	);
 }
 
 export default memo(Clients);
